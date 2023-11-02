a9d3e44b220d720f427846146c6d999eba6edd8d
Harshal2703 <harshalchaudhari2703@gmail.com>
2023-11-02 13:48:25 +0530

    3.1.2 (test)

--- app/signin/page.js
+++ app/signin/page.js
@@ -24,7 +24,7 @@ export default function SignInPage() {
                     </div>
                 </div>
                 <div className="text-[60px] font-bold w-[50%] text-center">
-                    {"World's First Online Chess Platform with Video Chatting sjswkw"}
+                    {"World's First Online Chess Platform with Video Chatting Feature"}
                 </div>
             </div>
         </>
